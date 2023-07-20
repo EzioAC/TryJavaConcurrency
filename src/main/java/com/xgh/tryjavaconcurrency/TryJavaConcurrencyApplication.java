823c71447522fcc0e5528bc00f8de4eeadca0d92
EzioXiang <1094708529@qq.com>
2023-07-20 08:17:32 +0800

    TryJavaConcurrency-可见性

---
+++ src/main/java/com/xgh/tryjavaconcurrency/TryJavaConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.xgh.tryjavaconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryJavaConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryJavaConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgh/tryjavaconcurrency/TryJavaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgh.tryjavaconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryJavaConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
